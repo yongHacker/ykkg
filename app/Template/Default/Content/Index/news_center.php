6dbfb1120368b83c464170943de21ba22976d8aa
twilightkk <29631880+twilightkk@users.noreply.github.com>
2018-10-18 10:50:53 +0800

    样式修改

--- app/Template/Default/Content/Index/news_center.php
+++ app/Template/Default/Content/Index/news_center.php
@@ -42,15 +42,17 @@
     <!-- news_center -->
     <div class="news-center container">
         <div class="row">
+            <volist name="video" id="vo">
             <!--要弄一个遮罩层，图片要覆盖在视频上方-->
             <div class="col-6">
                 <div class="embed-responsive">
                     <!--<video src="" controls="true" poster="{$vo.img}" preload="auto"> 抱歉，您的浏览器不支持内嵌视频</video>-->
                     <!-- <embed src="http://player.video.iqiyi.com/04d4ed8fe6ab306ff2482b4df1d0ea39/0/0/v_19rr65m6jw.swf-albumId=1413828800-tvId=1413828800-isPurchase=0-cnId=25" allowFullScreen='true' quality='high' width='480' height='350' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed> -->
-                    <?php echo htmlspecialchars_decode($vo['url']) ?>
+                    <?php echo htmlspecialchars_decode($vo['url']); ?>
                 </div>
                 <h3>{$vo.title_zh}</h3>
             </div>
+            </volist>
         </div>
         <div class="pagination">
            <!--  <ul class="pagination">
