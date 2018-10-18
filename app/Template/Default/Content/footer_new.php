d4bfab4e4fda678780b5d17615f90dfe7bc5542d
twilightkk <29631880+twilightkk@users.noreply.github.com>
2018-10-18 17:30:17 +0800

    图片添加

--- app/Template/Default/Content/footer_new.php
+++ app/Template/Default/Content/footer_new.php
@@ -10,7 +10,7 @@
             <div class="container">
                 <p class="contact"><span><a href="#">使用条款</a></span><span><a href="#">联系我们</a></span></p>
                 <p>Copyright © 2016 All Reserved 版权所有：浙江永坤控股有限公司 <a href="#">浙ICP备13030423号</a></p>
-                <div class="click"><img src="{$config_siteurl}statics/default/ykkg/img/1.jpg" width="18" height="20" alt=""><p>浙公网安备 33010402002764号</p></div>
+                <div class="click"><img src="{$config_siteurl}statics/default/ykkg/img/small/law.png" width="18" height="20" alt=""><p>浙公网安备 33010402002764号</p></div>
             </div>
         </div>
     </footer>
